753bee2e4e1d0fab72d7e0d94bb43305c096f7d0
Robert Evans <rob@Roberts-MacBook-Pro.local>
2024-10-09 12:08:47 +0100

    feat: fixing linting erros

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          X for profound shitposting
+          for profound shitposting
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
