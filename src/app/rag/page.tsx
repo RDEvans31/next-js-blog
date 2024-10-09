bf3f7ff1e301a6168819aff0d21ad38b905b73d4
Robert Evans <rob@Roberts-MacBook-Pro.local>
2024-10-09 12:26:46 +0100

    fix: fix copy and added husky and rag

---
+++ src/app/rag/page.tsx
@@ -0,0 +1,13 @@
+export default async function Posts() {
+  return (
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-8 grid-rows-2 items-center sm:items-start">
+        <div>
+          I built an RAG system so I could have an AI powered question-answering
+          system for all my sources of information. It&apos;s not deployed yet
+          but will be soon.
+        </div>
+      </main>
+    </div>
+  );
+}
