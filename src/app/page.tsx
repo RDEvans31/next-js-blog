bc05e1eddd0d922027bbde24d7cb0bd503233e23
Robert Evans <rob@Roberts-MacBook-Pro.local>
2024-10-09 17:45:54 +0100

    fix: fix link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://wildmindscommunity.com"
+          href="https://wwww.wildmindscommunity.com"
           target="_blank"
           rel="noopener noreferrer"
         >
