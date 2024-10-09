b12045eec4593ca46139f17e6576c00eb9a5e210
Robert Evans <rob@Roberts-MacBook-Pro.local>
2024-10-09 12:25:42 +0100

    fix: fixing some warnings and links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,28 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Navbar() {
-    return (
-        <div className="flex justify-between items-center px-12 py-4 border-b">
-            <div className="flex items-center gap-4">
-                <Image src="/nomadic_lifter_logo.png" alt="logo" width={40} height={40} className="rounded-full" />
-                <a href="/" className="text-lg font-bold">NomadicLifter</a>
-            </div>
-            <div className="flex items-center gap-4">
-                <a href="/posts" className="hover:underline">Posts</a>
-                <a href="/posts" className="hover:underline">Ask a question</a>
-            </div>
-        </div>
-    )
-}   
+  return (
+    <div className="flex justify-between items-center px-12 py-4 border-b">
+      <div className="flex items-center gap-4">
+        <Image
+          src="/nomadic_lifter_logo.png"
+          alt="logo"
+          width={40}
+          height={40}
+          className="rounded-full"
+        />
+        <a href="/" className="text-lg font-bold">
+          NomadicLifter
+        </a>
+      </div>
+      <div className="flex items-center gap-4">
+        <a href="/posts" className="hover:underline">
+          Posts
+        </a>
+        <a href="/rag" className="hover:underline">
+          Ask a question
+        </a>
+      </div>
+    </div>
+  );
+}
